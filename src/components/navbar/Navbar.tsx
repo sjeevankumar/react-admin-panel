0583670900e3ffe14a1db0a9868a1208a3d56790
sjeevankumar <sjeevankumar89@gmail.com>
2024-08-31 14:50:56 +0530

    feat:implement navbar design

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,27 @@
-import "./navbar.scss";
+import "./navbar.scss"
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>
-};
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>lamadmin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon"/>
+        <img src="/app.svg" alt="" className="icon"/>
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="https://images.unsplash.com/photo-1719937050601-969f4f25d060?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon"/>
+      </div>
+    </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
