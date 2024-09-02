97628122fe1fd9905cac55413899922ba9a0a4d0
sjeevankumar <sjeevankumar89@gmail.com>
2024-09-02 17:20:13 +0530

    feat:add dataTable in users page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,61 @@
+import "./users.scss"
+import { GridColDef } from "@mui/x-data-grid"
+import DataTable from "../../components/dataTable/DataTable"
+import { userRows } from "../../data"
+
 const Users = () => {
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "avatar",
+      headerName: "Avatar",
+      width: 100,
+      renderCell: (params) => {
+        return <img src={params.row.img || "/noavatar.png"} />
+      },
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 100,
+      type: "boolean",
+    },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (value, row) =>
+        `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+  ]
   return (
-    <div>
-      Users
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
     </div>
   )
 }
