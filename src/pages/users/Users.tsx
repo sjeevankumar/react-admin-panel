d8cbb8ad8d15f9e1b3e04732b6b37b31e5d173ba
sjeevankumar <sjeevankumar89@gmail.com>
2024-09-05 17:26:09 +0530

    feat:implement add modal in users page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -2,60 +2,66 @@ import "./users.scss"
 import { GridColDef } from "@mui/x-data-grid"
 import DataTable from "../../components/dataTable/DataTable"
 import { userRows } from "../../data"
+import { useState } from "react"
+import Add from "../../components/add/Add"
 
 const Users = () => {
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID", width: 90 },
     {
-      field: "avatar",
+      field: "img",
       headerName: "Avatar",
       width: 100,
       renderCell: (params) => {
-        return <img src={params.row.img || "/noavatar.png"} />
+        return <img src={params.row.img || "/noavatar.png"} alt="" />
       },
     },
     {
-      field: "status",
-      headerName: "Status",
-      width: 100,
-      type: "boolean",
-    },
-    {
       field: "firstName",
+      type: "string",
       headerName: "First name",
       width: 150,
-      editable: true,
     },
     {
       field: "lastName",
+      type: "string",
       headerName: "Last name",
       width: 150,
-      editable: true,
     },
     {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 110,
-      editable: true,
+      field: "email",
+      type: "string",
+      headerName: "Email",
+      width: 200,
     },
     {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (value, row) =>
-        `${row.firstName || ""} ${row.lastName || ""}`,
+      field: "phone",
+      type: "string",
+      headerName: "Phone",
+      width: 200,
+    },
+    {
+      field: "createdAt",
+      headerName: "Created At",
+      width: 200,
+      type: "string",
+    },
+    {
+      field: "verified",
+      headerName: "Verified",
+      width: 150,
+      type: "boolean",
     },
   ]
+  const [open, setOpen] = useState(false)
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add New User</button>
+        <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
       <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   )
 }
